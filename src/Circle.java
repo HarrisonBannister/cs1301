7728e84b22e3141c0acbeeedf08a532868b63582
Harrison Bannister <seal@localhost.localdomain>
2014-10-20 11:28:46 -0400

    first commit

---
+++ src/Circle.java
@@ -0,0 +1,148 @@
+
+/**
+* Circle.java
+* Author: Harrison Bannister
+* Submission Date: [Submission date here]
+*
+* Purpose: This program leads the user through an interactive fiction
+* game, "The Adventure of the Cave of Redundancy Adventure".
+*
+* Statement of Academic Honesty:
+*
+* The following code represents my own work. I have neither
+* received nor given inappropriate assistance. I have not copied
+* or modified code from any source other than the course webpage
+* or the course textbook. I recognize that any unauthorized
+* assistance or plagiarism will be handled in accordance with
+* the University of Georgia's Academic Honesty Policy and the
+* policies of this course. I recognize that my work is based
+* on an assignment created by the Department of Computer
+* Science at the University of Georgia. Any publishing
+* or posting of source code for this project is strictly
+* prohibited unless you have written consent from the Department
+* of Computer Science at the University of Georgia.
+*/
+
+//*******************************************************
+// Circle.java
+//
+// 
+//*******************************************************
+public class Circle {
+	
+
+	__________________________       // declare the private double instance  radius
+	__________________________       // declare the private double instance  x
+	__________________________       // declare the private double instance  y
+	
+	//----------------------------------------------
+	// Class Constructor: set the initial values of
+	//                    the instance variables
+	//                    for this circle
+	//----------------------------------------------	
+	public Circle(double x, double y,double  radius) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;  	   	
+	}
+	
+	
+	//----------------------------------------------
+	// getX - returns the value of x
+	//----------------------------------------------
+	public ______ getX() {
+		
+		// Your code goes here	
+	}
+	
+
+	//----------------------------------------------
+	// getY - returns the value of y
+	//----------------------------------------------
+	public ______ getY() {
+		
+		// Your code goes here	
+	}
+	
+	//----------------------------------------------
+	// getRadius - returns the value of radius
+	//----------------------------------------------
+	public ______ getRadius() {
+		
+		// Your code goes here	
+	}
+
+	//----------------------------------------------
+	// setX - assigns a new value to x
+	//----------------------------------------------
+	public ______ setX(_______________) {
+		
+		// Your code goes here	
+	}
+	
+
+	//----------------------------------------------
+	// setY - assigns a new value to y
+	//----------------------------------------------
+	public ______ setY(_______________) {
+		
+		// Your code goes here	
+	}	
+	
+	
+	//----------------------------------------------
+	// setRadius - assigns a new value to radius
+	//----------------------------------------------
+	public ______ setRadius(_______________) {
+		
+		// Your code goes here	
+	}
+	
+	//--------------------------------------------------------
+	// diameter - calculates the diameter of the circle
+	//--------------------------------------------------------
+	public ______ diameter() {
+		
+		// Your code goes here	
+	}
+	
+
+	//--------------------------------------------------------
+	// area - returns the area of the circle
+	//--------------------------------------------------------
+	public ______ area() {
+		
+		// Your code goes here	
+	}
+
+	//--------------------------------------------------------
+	// perimeter - returns the perimeter of the circle
+	//--------------------------------------------------------
+	public ______ perimeter() {
+		
+		// Your code goes here	
+	}
+	
+	//--------------------------------------------------------
+	// isUnitCircle - return true if the radius of this circle
+	//                is 1 and its center is (0,0) and false
+	//      	      otherwise.
+	//--------------------------------------------------------
+	public ______ isUnitCircle() {
+		
+		// Your code goes here	
+	}
+	
+	
+	//--------------------------------------------------------
+	// toString - return a String representation of
+	//            this circle in the following format:
+	//            center:(x,y)
+	//            radius: r
+	//--------------------------------------------------------
+	public ______ toString() {
+		
+		// Your code goes here	
+	}
+
+}
